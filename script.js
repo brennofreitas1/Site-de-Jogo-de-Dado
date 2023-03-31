95ef5419811d0a0aa1308e8af682f5a4576d0558
brennofreitas1 <112783205+brennofreitas1@users.noreply.github.com>
2023-03-31 14:01:28 -0300

    1 binaries uploaded

--- script.js
+++ script.js
@@ -2,12 +2,8 @@ const Buttom = document.getElementById('teclar');
 const chamar = document.getElementById('chamar');
 
 const btRodarDado = () => {
+  let img = []
   const dado = Math.floor(Math.random() * 6) + 1;
   chamar.innerHTML = dado;
 };
 Buttom.onclick = btRodarDado;
-
-if (dado = 1){
-   dado. innerHTML = <img src=".png" alt=""/>
-}
-//Brenno Henrique Freitas Barbosa
